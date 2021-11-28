d74b71c939523b1836c113c053f928b692916c1b
Catalyst <danielduyilemi@gmail.com>
2021-11-28 18:36:57 +0100

    final deploy 1

--- models/images.js
+++ models/images.js
@@ -1,10 +1,17 @@
 var mongoose = require('mongoose');
 var imageSchema = new mongoose.Schema({
+	id:{
+		type: mongoose.Types.ObjectId,
+		ref:"Image"
+	},
+	name: String,
     img:
     {
         data: Buffer,
         contentType: String
-    }
+    },
+    price: String,
+    purchaseUrl: String
 });
 Image = mongoose.model("Image",imageSchema);
 module.exports = Image;
